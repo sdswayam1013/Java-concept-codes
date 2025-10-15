800a06f42ccb6397e43e9c811ee8a24e0559296f
sdswayam1013 <sshaileshdas97@gmail.com>
2025-10-15 16:54:25 +0530

    use of inner class and basic thred creation

--- Java Codes/BasicThread.java
+++ Java Codes/BasicThread.java
@@ -3,9 +3,18 @@ public class BasicThread{
 
     public static void main(String[]args){
         //creating runnable using the inner class
-       Runnable rn = new Runnable() {
-        @Override
-         public void run(){
+       Runnable rn = new Runnable() {                   // before java 8: Interface Runnable
+                                                          //{ void run();}
+                                                          
+         @Override                                         //class Runnable implements Runnable{
+        //                                                    @Override
+        //                                                    public void run(){
+        //                                                     System.out.println("Hello from the thread" + Thread.currentThread.getName());
+        //                                                    }
+       //                                                              }
+        //                                                    Runnable rn = new Runnable();//upcasting
+        //                                                         }
+         public void run(){ 
             System.out.println("Hello from the thread " + Thread.currentThread().getName());
          }
                 
